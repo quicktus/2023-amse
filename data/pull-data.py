67dcfb971540342c313b52b83df3b8b98ef0aa79
quicktus <quintuswenker@gmail.com>
2023-05-30 11:01:53 +0200

    Pipeline (#13)
    * add pipeline for Datasource2: DWD Climate Data
    
    Co-authored-by: StealWonders <6114858+StealWonders@users.noreply.github.com>
    
    * add python requirements.txt
    
    * add pipeline for Datasource1: Spotify (kaggle)
    
    * add pipeline --clear option for rebuild
    
    * optimize pipeline performance
    
    * refactor pipeline
    
    * add error handling
    
    * rename columns and link documentation
    
    * add spotify track metadata for more analysis potential
    
    * accommodate for Spotify API rate limit

--- data/pull-data.py
+++ data/pull-data.py
@@ -2,6 +2,8 @@
 
 # * Datasource1: Spotify daily charts per country (kaggle)
 #    -> https://www.kaggle.com/datasets/pepepython/spotify-huge-database-daily-charts-over-3-years
+#   with additional metadata from Spotify API
+#    -> https://developer.spotify.com/documentation/web-api/
 
 # * Datasource2: Climate Data (DWD)
 #   - Rain           https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/more_precip/
@@ -13,9 +15,11 @@
 #       clean it by deleting and re-downloading all data files and re-building the SQLite db.
 
 import os
+import time
 import pandas as pd
 import re
 import shutil
+import spotipy
 import sqlalchemy as sa
 import sys
 import zipfile
@@ -36,7 +40,7 @@ FTP_URI: str = "opendata.dwd.de"
 engine = sa.create_engine(DB_CONNECTION_URI)
 
 def main():
-   # Clean data if "--clean" flag is present
+    # Clean data if "--clean" flag is present
     if "--clean" in sys.argv:
         clean_data()
 
@@ -52,6 +56,7 @@ def main():
         else:
             download_spotify_data(spotify)
         extract_spotify_data_to_db(spotify)
+        get_spotify_metadata(spotify)
 
     # Download and extract weather data
     for data_src in data_sources:
@@ -230,6 +235,97 @@ def extract_spotify_data_to_db(spotify: str):
         log((f"Could not extract {spotify}" + " "*7), "error")
 
     log((f"Extracted {spotify}" + " "*15), "success")
+
+
+def get_spotify_metadata(spotify: str):
+    """Gets the metadata for each track through the Spotify API."""
+
+    print(log(f"Getting {spotify} track metadata", "status", ret_str=True), end= "\r")
+
+    # get a list of all tracks in the database
+    tracks = None
+    try:
+        connection = engine.connect()
+        tracks = connection.execute(sa.text(f"SELECT DISTINCT uri FROM {spotify};")).fetchall()
+        assert tracks is not None
+    except:
+        log(f"Could not get {spotify} from database", "error")
+        return None
+
+    tracks = [track_row[0].strip() for track_row in tracks]
+    try:
+        tracks.remove("N\A")
+        tracks.remove("#")
+    except:
+        pass # ignore
+
+    tracks_100 = [tracks[i:i + 100] for i in range(0, len(tracks), 100)]
+
+    # Read the Spotify credentials from a file
+    sp, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET = None, None, None
+    try:
+        lines = open("./spotify_credentials.txt", "r").readlines()
+        SPOTIFY_CLIENT_ID = lines[0].strip()
+        SPOTIFY_CLIENT_SECRET = lines[1].strip()
+    except:
+        log("Could not read Spotify credentials file", "error")
+        return None
+
+    # Authenticate with the Spotify API
+    try:
+        auth_manager = spotipy.oauth2.SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
+        sp = spotipy.Spotify(client_credentials_manager=auth_manager)
+    except:
+        log("Could not authenticate with Spotify API", "error")
+        return None
+
+    # Get the metadata for each track
+    audio_features = pd.DataFrame(columns = ["uri", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "duration_ms"])
+    try:
+        desc = log(f"Getting {spotify} track metadata from server ", "status", ret_str=True)
+        progress = track(tracks_100, description=desc, transient=True)
+        for spotify_track in progress:
+            while True:
+                try:
+                    new_features = sp.audio_features(spotify_track)
+                    new_features = pd.DataFrame.from_dict(new_features)
+                    audio_features = pd.concat([audio_features, new_features], join="inner", sort=True)
+                    break
+                except:
+                    # The Spotify API rate limit is not publicly documented, but this seems to work
+                    time.sleep(20)
+    except:
+        log(f"Could not get {spotify} track metadata from server", "error")
+        return None
+
+    try:
+        # Store metadata in the database
+        audio_features.to_sql("audio_features", engine, if_exists="replace", index=False)
+    except:
+        log(f"Could not store {spotify} track metadata in database", "error")
+        return None
+
+
+    # brief summary of spotify's audio features:
+    # "acousticness": A measure from 0.0 to 1.0 indicating the confidence of whether a track is acoustic, with 1.0 representing high confidence in its acoustic nature.
+    # "analysis_url": A URL providing access to the full audio analysis of a track.
+    # "danceability": A number from 0.0 to 1.0 describing how suitable a track is for dancing based on tempo, rhythm stability, beat strength, and regularity.
+    # "duration_ms": The duration of the track in milliseconds.
+    # "energy": A measure from 0.0 to 1.0 representing the intensity and activity level of a track, with higher values indicating greater energy.
+    # "id": The unique Spotify ID assigned to a track.
+    # "instrumentalness": A measure from 0.0 to 1.0 predicting the likelihood of a track containing no vocal content, with higher values suggesting instrumental tracks.
+    # "key": The key in which the track is performed, mapped to standard Pitch Class notation.
+    # "liveness": A number from 0.0 to 1.0 indicating the probability of a track being performed live, with higher values suggesting a live recording.
+    # "loudness": The overall loudness of a track in decibels (dB), ranging from -60 to 0 dB.
+    # "mode": An integer representing the modality (major or minor) of a track, with 1 for major and 0 for minor.
+    # "speechiness": A number from 0.0 to 1.0 indicating the presence of spoken words in a track, with higher values suggesting more speech-like recordings.
+    # "tempo": The estimated tempo of a track in beats per minute (BPM).
+    # "time_signature": An estimated time signature specifying the number of beats in each bar, ranging from 3 to 7.
+    # "track_href": A link to the Web API endpoint providing detailed information about the track.
+    # "type": The object type, which is "audio_features" for audio feature objects.
+    # "uri": The Spotify URI for the track.
+    # "valence": A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track, with higher values indicating more positive emotions.
+    # for a full description of the audio features, see https://developer.spotify.com/documentation/web-api/reference/get-audio-features
 
 
 if __name__ == "__main__":
