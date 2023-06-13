cd396ebc01f1a66cfda3bb72b9d9a141b0956d29
Kilian Wenker <kilian.wenker@fau.de>
2023-06-14 00:25:49 +0200

    add dbg print

--- data/pull-data.py
+++ data/pull-data.py
@@ -219,6 +219,7 @@ def download_spotify_data(spotify: str):
         # use gh secrets
         KAGGLE_KEY = os.environ.get("KAGGLE_KEY")
         KAGGLE_USERNAME = os.environ.get("KAGGLE_USERNAME")
+        print(len(KAGGLE_KEY), len(KAGGLE_USERNAME))
         json_str = '{"username":"' + KAGGLE_USERNAME + '","key":"' + KAGGLE_KEY + '"}'
         os.makedirs("/home/runner/.kaggle", exist_ok=True)
         with open("/home/runner/.kaggle/kaggle.json", 'w') as file:
