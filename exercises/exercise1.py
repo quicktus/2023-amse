c6bc768e72048c5065903ccc14b291b506ce257a
Kilian Wenker <kilian.wenker@fau.de>
2023-06-14 11:18:21 +0200

    add lint config and lint

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -8,6 +8,7 @@ import sqlalchemy as sa
 
 SOURCE_URI: str = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
 
+
 def main():
     # download the CSV file from SOURCE_URI
     df = pd.read_csv(SOURCE_URI, sep=";")
@@ -32,20 +33,21 @@ def main():
     # Write the data into the SQLite database and assign fitting types using pandas
     engine = sa.create_engine("sqlite:///airports.sqlite")
     df.to_sql("airports", engine, if_exists="replace", index=False, dtype={
-            "column_1"  : sa.types.INTEGER, # Unique OpenFlights identifier for this airport.
-            "column_2"  : sa.types.TEXT,    # Name of airport. May or may not contain the City name.
-            "column_3"  : sa.types.TEXT,    # Main city served by airport. May be spelled differently from Name.
-            "column_4"  : sa.types.TEXT,    # Country or territory where airport is located. See Countries to cross-reference to ISO 3166-1 codes. 
-            "column_5"  : sa.types.TEXT,    # 3-letter IATA code. Null if not assigned/unknown.
-            "column_6"  : sa.types.TEXT,    # 4-letter ICAO code. Null if not assigned.
-            "column_7"  : sa.types.FLOAT,   # Decimal degrees, usually to six significant digits. Negative is South, positive is North.
-            "column_8"  : sa.types.FLOAT,   # Decimal degrees, usually to six significant digits. Negative is West, positive is East.
-            "column_9"  : sa.types.INTEGER, # In feet.
-            "column_10" : sa.types.FLOAT,   # Hours offset from UTC. Fractional hours are expressed as decimals, eg. India is 5.5.
-            "column_11" : sa.types.CHAR,    # Daylight savings time. One of E (Europe), A (US/Canada), S (South America), O (Australia), Z (New Zealand), N (None) or U (Unknown).
-            "column_12" : sa.types.TEXT,    # Timezone in "tz" (Olson) format, eg. "America/Los_Angeles".
-            "geo_punkt" : sa.types.TEXT,    # (redundant) Latitude and longitude separated by a comma
-        })
+        "column_1"  : sa.types.INTEGER,      # Unique OpenFlights identifier for this airport.
+        "column_2"  : sa.types.TEXT,     # Name of airport. May or may not contain the City name.
+        "column_3"  : sa.types.TEXT,     # Main city served by airport. May be spelled differently from Name.
+        "column_4"  : sa.types.TEXT,     # Country or territory where airport is located. See Countries to cross-reference to ISO 3166-1 codes.
+        "column_5"  : sa.types.TEXT,     # 3-letter IATA code. Null if not assigned/unknown.
+        "column_6"  : sa.types.TEXT,     # 4-letter ICAO code. Null if not assigned.
+        "column_7"  : sa.types.FLOAT,    # Decimal degrees, usually to six significant digits. Negative is South, positive is North.
+        "column_8"  : sa.types.FLOAT,    # Decimal degrees, usually to six significant digits. Negative is West, positive is East.
+        "column_9"  : sa.types.INTEGER,  # In feet.
+        "column_10" : sa.types.FLOAT,    # Hours offset from UTC. Fractional hours are expressed as decimals, eg. India is 5.5.
+        "column_11" : sa.types.CHAR,     # Daylight savings time. One of E (Europe), A (US/Canada), S (South America), O (Australia), Z (New Zealand), N (None) or U (Unknown).
+        "column_12" : sa.types.TEXT,     # Timezone in "tz" (Olson) format, eg. "America/Los_Angeles".
+        "geo_punkt" : sa.types.TEXT,     # (redundant) Latitude and longitude separated by a comma
+    })
+
 
 if __name__ == "__main__":
     main()
